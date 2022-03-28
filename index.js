fcb766f33ad4ed0ea4b35cd894cc88e40d877253
SergioIznaolaDeveloper <sergioiznaola@gmail.com>
2022-03-28 16:29:39 +0200

    add_pugs_working_and_css

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
   
-  module.exports = server;
+  // module.exports = server;
--- routes/routes_films.js
+++ routes/routes_films.js
@@ -1,9 +1,8 @@
-const express = require('express;');
+const express = require('express');
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    
     res.render('home.pug')
 })
 router.get('/dashboard', (req, res) => {
