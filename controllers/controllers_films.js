8aaf0dbd03606a4b6ccf6244b089ad1f0dd6bb7b
Ricardo <Ricardontutumu@gmail.com>
2022-04-04 14:50:16 +0100

    FIX|add user controller and test create user function

--- controllers/controllers_films.js
+++ controllers/controllers_films.js
@@ -1,6 +1,5 @@
 const films = require("../utils/utils_films");
 const Movie = require("../models/models_films");
-const user = require("../models/models_users.js");
 
 // obtener pelis
 
@@ -71,16 +70,6 @@ const editFilms = async (req, res) => {
   }
 };
 
-// crear usuario en  sql
-
-const createUser = async (req, res) => {
-  try {
-    let datos = await user.createUser(req.body);
-    res.status(201).json(datos);
-  } catch (error) {
-    console.log(`ERROR: ${error.stack}`);
-  }
-};
 
 // crear movie por el admin
 const createMovie = async (req, res) => {
@@ -150,7 +139,6 @@ const film = {
   getFilms,
   createMovie,
   getAllMovies,
-  createUser,
   deleteMovie,
   getFavorites,
   getAdminFilms,
