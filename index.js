aa7ebc0809a24fea903cffad11428874f9487548
Ricardo <Ricardontutumu@gmail.com>
2022-04-18 15:59:06 +0100

    Commented useless line

--- index.js
+++ index.js
@@ -33,43 +33,43 @@ app.use("/",filmsRouter);
 // ************  Google auth  ************** //
 
 // For an actual app you should configure this with an experation time, better keys, proxy and secure
-app.use(cookieSession({
-  name: 'moviesClient-session',
-  keys: ['key1', 'key2']
-}))
+// app.use(cookieSession({
+//   name: 'moviesClient-session',
+//   keys: ['key1', 'key2']
+// }))
 
 // Auth middleware that checks if the user is logged in
-const isLoggedIn = (req, res, next) => {
-  if (req.user) {
-      next();
-  } else {
-      res.sendStatus(401);
-  }
-}
+// const isLoggedIn = (req, res, next) => {
+//   if (req.user) {
+//       next();
+//   } else {
+//       res.sendStatus(401);
+//   }
+// }
 
-// Initializes passport and passport sessions
-app.use(passport.initialize());
-app.use(passport.session());
+// // Initializes passport and passport sessions
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-// Auth Routes
-app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-app.get('/failed', (req, res) => res.send('You Failed to log in!'))
+// // Auth Routes
+// app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// app.get('/failed', (req, res) => res.send('You Failed to log in!'))
 
-// In this route you can see that if the user is logged in u can acess his info in: req.user
-app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.displayName}!`))
+// // In this route you can see that if the user is logged in u can acess his info in: req.user
+// app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.displayName}!`))
 
-app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/good');
-  }
-);
+// app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+//   function(req, res) {
+//     // Successful authentication, redirect home.
+//     res.redirect('/good');
+//   }
+// );
 
-app.get('/logout', (req, res) => {
-    req.session = null;
-    req.logout();
-    res.redirect('/');
-})
+// app.get('/logout', (req, res) => {
+//     req.session = null;
+//     req.logout();
+//     res.redirect('/');
+// })
 
 
 
--- middlewares/googleAuth.js
+++ middlewares/googleAuth.js
@@ -1,28 +1,28 @@
-const createUser = require('../controllers/controllers_users.js');
+// const createUser = require('../controllers/controllers_users.js');
 
-const googleAuth = async (req, res, next) => {
-    if (!req.user) {
-        return next(error)
-    }
+// const googleAuth = async (req, res, next) => {
+//     if (!req.user) {
+//         return next(error)
+//     }
 
-    try {
-        const userData = { name: req.user.displayName, email: req.user.email, password: require('crypto').randomBytes(64).toString('hex') }
-        // Comprobamos que el usuario no esté ya en la DB:
-        const foundUser = await findUserEmail(userData.email);
-        if (foundUser) {
-            req.user = foundUser;
-            return next();
-        }
-        // Crea usuario en DB
-        const newUser = await createUser(userData);
+//     try {
+//         const userData = { name: req.user.displayName, email: req.user.email, password: require('crypto').randomBytes(64).toString('hex') }
+//         // Comprobamos que el usuario no esté ya en la DB:
+//         const foundUser = await findUserEmail(userData.email);
+//         if (foundUser) {
+//             req.user = foundUser;
+//             return next();
+//         }
+//         // Crea usuario en DB
+//         const newUser = await createUser(userData);
 
-        req.user.user_id = newUser.user_id;
+//         req.user.user_id = newUser.user_id;
 
-        return next()
-    }
-    catch (error) {
-        return next(error)
-    }
-}
+//         return next()
+//     }
+//     catch (error) {
+//         return next(error)
+//     }
+// }
 
 // module.exports = googleAuth;
